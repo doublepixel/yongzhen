d7cd01c0c6d120af2156d43636d02aaace5bb0a8
lvyz <lvyz0207@163.com>
2018-03-23 13:15:51 +0800

    fix

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
-ALLOWED_HOSTS = [.xiaotian.com]
+ALLOWED_HOSTS = ['.xiaotian.com']
 
 # Application definition
 
